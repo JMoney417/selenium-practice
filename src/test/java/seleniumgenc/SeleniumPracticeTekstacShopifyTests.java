9732e1ab872cf079ef695e7f64b585bb4e5d9fe2
M_688776 <jason.monroe@cognizant.com>
2021-02-23 11:10:36 -0600

    POM added

--- src/test/java/seleniumgenc/SeleniumPracticeTekstacShopifyTests.java
+++ src/test/java/seleniumgenc/SeleniumPracticeTekstacShopifyTests.java
@@ -1,19 +1,27 @@
 package seleniumgenc;
 
-import org.junit.Assert;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
+import seleniumgenc.Utils.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import seleniumgenc.pages.ShopifyRegisterPage;
+import seleniumgenc.pages.ShopifyRegisterPageBuilder;
 
 public class SeleniumPracticeTekstacShopifyTests {
 
-	private final String BASE_URL = "https://webapps.tekstac.com/Shopify";
+	private ShopifyRegisterPageBuilder shopifyRegisterPageBuilder;
 	private WebDriver driver;
 	
 	@BeforeTest
@@ -21,21 +29,66 @@ public class SeleniumPracticeTekstacShopifyTests {
 		WebDriverManager.chromedriver().setup();
 	}
 	
-	@Test
-	public void shopifyRegisterFormContainsAllTextboxes(){
-		//Arrange
-		driver = new ChromeDriver();
-		driver.get(BASE_URL);
-		
-		//Act
-		WebElement firstNameTextBox = driver.findElement(By.id("firstname"));
-		
-		//Assert
-		Assert.assertTrue(firstNameTextBox.isDisplayed());
+	@BeforeMethod
+	public void beforeMethod(){
+		shopifyRegisterPageBuilder = new ShopifyRegisterPageBuilder(new ShopifyRegisterPage(new ChromeDriver()));
 	}
 	
-	@AfterTest
+	@Test
+	public void shopifyRegisterFormContainsAllTextboxes(){
+		
+		Assert.assertTrue(shopifyRegisterPageBuilder.build().getFirstNameTextBox().isDisplayed());
+		Assert.assertTrue(shopifyRegisterPageBuilder.build().getLastNameTextBox().isDisplayed());
+		Assert.assertTrue(shopifyRegisterPageBuilder.build().getUserNameTextBox().isDisplayed());
+		Assert.assertTrue(shopifyRegisterPageBuilder.build().getPasswordNameTextBox().isDisplayed());
+		
+	}
+	
+	@Test
+	public void submittingFormAddsInfoToBottomTable(){
+		
+		//Arrange/Act
+		ShopifyRegisterPage page = shopifyRegisterPageBuilder
+				.withFirstName("Jason")
+				.withLastName("Monroe")
+				.withUserName("JMoney")
+				.withCity(City.valueOf("Coimbatore"))
+				.withGender(Gender.valueOf("Male"))
+				.withPassword("password-1").build();
+		
+		page.clickRegisterButton();
+		
+		//find table and view rows
+		WebElement table = page.getTable();
+		List<WebElement> rows = table.findElements(By.tagName("tr"));
+		
+		String expected = "Jason Monroe JMoney Coimbatore";
+		String actual = rows.get(rows.size()-1).getText();
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void incomplete_form_issues_error(){
+		
+		//Arrange/Act
+		ShopifyRegisterPage page = shopifyRegisterPageBuilder
+				.withFirstName("Jason")
+				.withLastName("Monroe")
+				.withUserName("JMoney")
+				.withCity(City.valueOf("Coimbatore"))
+				.withGender(Gender.valueOf("Male")).build();
+		
+		page.clickRegisterButton();
+		
+		String expectedError = "Passwd can't be blank";
+		String actualError = page.getErrorMessages();
+		Assert.assertEquals(actualError, expectedError);
+		
+	}
+	
+	
+	@AfterMethod
 	public void afterTest(){
-		driver.quit();
+		shopifyRegisterPageBuilder.quitDriver();
 	}
 }
